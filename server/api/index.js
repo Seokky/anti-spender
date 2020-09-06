3ae5382ef14faefb2c9e41fb42d0b8d83746e309
Maxim Noverin <noverin@yandex.ru>
2020-09-06 22:53:48 +0500

    unnecessary server logic removed

--- server/api/index.js
+++ server/api/index.js
@@ -1,26 +1,9 @@
 const express = require('express')
-const https = require('https')
 const app = express()
 
-const verifyToken = (token, callbackSuccess, callbackError) => {
-  const googleTokenInfoURL = 'https://www.googleapis.com/oauth2/v1/tokeninfo';
+// app.get('', (req, res) => {
 
-  https.get(`${googleTokenInfoURL}?access_token=${token}`, ({ statusCode }) => {
-    if (statusCode === 200) {
-      callbackSuccess()
-    } else {
-      callbackError()
-    }
-  })
-}
-
-app.get('/verify/:token', (req, res) => {
-  verifyToken(
-    req.params.token,
-    () => { res.send('success') },
-    () => { res.send('error') }
-  )
-})
+// })
 
 module.exports = {
   path: '/api',
--- services/api.js
+++ services/api.js
@@ -1,8 +1,3 @@
 export default {
-  verifyToken: async (apiProvider) => {
-    const token = localStorage.getItem('auth._token.google').split(' ')[1]
-    const res = await apiProvider.$get(`/api/verify/${token}`)
 
-    return res
-  }
 }
